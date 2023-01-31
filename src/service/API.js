534c4d44a42b69408d9e1dfd629baff245387aa6
unknown <antunespedro222@gmail.com>
2023-01-31 16:21:54 -0300

    service

---
+++ src/service/API.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+// import AuthContext from "./auth";
+
+const url = "https://backend-listaleitura-production.up.railway.app/";
+// const url = "http://localhost:5000/";
+
+export function getUser(id) {
+  return axios.get( url + "usuario/" + id);
+}
+
+export function getUsers() {
+  return axios.get( url + "usuario/all");
+}
+
+export function addUser(nome, email, senha) {
+  return axios.post( url + "usuario/add", {
+    nome: nome,
+    email: email,
+    senha: senha,
+  });
+}
+
+export function delUser(id) {
+  return axios.delete(
+    url + "usuario/" + id
+  );
+}
+
+export function putUser(id, nome, email, senha) {
+  return axios.put(
+    url + "usuario/" + id,
+    {
+      nome: nome,
+      email: email,
+      senha: senha,
+    }
+  );
+}
+
+export function getLivro(id) {
+  return axios.get( url + "livro/" + id);
+}
+
+export function addLivro(
+  capa,
+  titulo,
+  subTitulo,
+  generoPrincipal,
+  generoSecundario,
+  sinopse,
+  paginasTotais,
+  authenticated
+) {
+  return axios.post( url + "livro/add", {
+    capa: capa,
+    titulo: titulo,
+    subTitulo: subTitulo,
+    generoPrincipal: generoPrincipal,
+    generoSecundario: generoSecundario,
+    sinopse: sinopse,
+    paginasLidas: 0,
+    paginasTotais: paginasTotais,
+    completo: false,
+    usuario: { id: authenticated },
+  });
+}
+
+export function delLivro(id) {
+  return axios.delete(
+    url+ "livro/" + id
+  );
+}
+
+export function putLivro(
+  id,
+  capa,
+  titulo,
+  subTitulo,
+  generoPrincipal,
+  generoSecundario,
+  sinopse,
+  paginasLidas,
+  paginasTotais,
+  rating,
+  completo,
+  authenticated
+) {
+  return axios.put( url + "livro/" + id, {
+    capa: capa,
+    titulo: titulo,
+    subTitulo: subTitulo,
+    generoPrincipal: generoPrincipal,
+    generoSecundario: generoSecundario,
+    sinopse: sinopse,
+    paginasLidas: paginasLidas,
+    paginasTotais: paginasTotais,
+    rating: rating,
+    completo: completo,
+    usuario: { id: authenticated },
+  });
+}
---
+++ src/service/auth.jsx
@@ -0,0 +1,33 @@
+import React, { useState, createContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Header from "../components/Header/Header";
+import Cadastro from "../pages/Login/Cadastro/Cadastro";
+
+const AuthContext = createContext({
+  authenticated: 0,
+  setAuthenticated: (auth) => {},
+});
+
+export const AuthProvider = ({ children }) => {
+  const auth = localStorage.getItem("login");
+  const [authenticated, setAuthenticated] = useState(auth);
+
+  return (
+    <AuthContext.Provider value={{ authenticated, setAuthenticated }}>
+      {authenticated > 0 ? (
+        children
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="*" element={<Login />} />
+            <Route path="/cadastrar" element={<Cadastro />} />
+          </Routes>
+        </>
+      )}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
