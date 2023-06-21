922c2762d08f52473c59d5bcc68b51087eb91767
unknown <antunespedro222@gmail.com>
2023-06-21 16:41:42 -0300

    fix: acesso a pag de login enquanto logado

--- src/pages/Rotes.jsx
+++ src/pages/Rotes.jsx
@@ -5,8 +5,6 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { AlertProvider } from "../context/Alert/alert";
 
 import Header from "../components/Header/Header";
-import Login from "./Login/Login";
-import SignIn from "./Login/Signin/Signin";
 import AddBook from "./AddBook/AddBook";
 import EditBook from "./EditBook/EditBook";
 import BookShelf from "./BookShelf/BookShelf";
@@ -27,8 +25,7 @@ export default function Rotas() {
         <AuthProvider>
           <Header />
             <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/signin" element={<SignIn />} />
+              <Route path="/" element={<BookShelf />} />
               <Route path="/addBook" element={<AddBook />} />
               <Route path="/shelf" element={<BookShelf />} />
               <Route path="/viewBook/:bookID" element={<ViewBook />} />
